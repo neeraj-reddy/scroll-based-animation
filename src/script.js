463053653626c931ca3ca3a0162e669d585530e3
neerredd <nrjrdy@gmail.com>
2021-11-11 13:03:51 +0530

    positioning objects

--- src/script.js
+++ src/script.js
@@ -40,12 +40,19 @@ const material = new THREE.MeshToonMaterial({
 });
 
 // Meshes
+const objectsDistance = 4;
+
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
 const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
 const mesh3 = new THREE.Mesh(
   new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
   material
 );
+
+mesh1.position.y = -objectsDistance * 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
+
 scene.add(mesh1, mesh2, mesh3);
 
 /**
